4a392f15e99376660840bdb53852a29369045e81
dgsw55 <m45a1@dgsw.hs.kr>
2025-11-24 12:22:31 +0900

    feat: annotation

--- src/main/java/com/tankboy/highknowledgeapi/domain/post/domain/entity/PostEntity.java
+++ src/main/java/com/tankboy/highknowledgeapi/domain/post/domain/entity/PostEntity.java
@@ -2,9 +2,14 @@ package com.tankboy.highknowledgeapi.domain.post.domain.entity;
 
 import com.tankboy.highknowledgeapi.global.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostEntity extends BaseEntity {
 }
