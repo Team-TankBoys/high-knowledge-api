30840fee06c7fbe1e8b7aa15dad7997513958203
jyjun_080808 <jinyj0217@gmail.com>
2025-11-24 12:30:42 +0900

    comment: add method description

--- src/main/java/com/tankboy/highknowledgeapi/domain/vote/presentation/controller/VoteController.java
+++ src/main/java/com/tankboy/highknowledgeapi/domain/vote/presentation/controller/VoteController.java
@@ -24,12 +24,18 @@ public class VoteController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void getVote(@PathVariable Long postId) {
         throw new NotImplementedException();
+
+        // 특정 postId를 가진 vote Entity를 모두 조회
     }
 
     @GetMapping("/{postId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancelVote(@PathVariable Long postId) {
         throw new NotImplementedException();
+
+        // Service 단계에서 현재 인증 중인 유저를 받아온 뒤 (VoteService의 createVote 메서드 참고)
+        // 해당 유저가 해당 postId에 대해 투표한 내역이 있는지 확인
+        // 있으면 vote 엔티티를 삭제
     }
 
 }
